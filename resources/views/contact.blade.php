5e2b7cd77554773b247e3f9abe7f1f5f1f7a4ecd
BintangAsyidqy <muhammadbintangasyidqy@smkwikrama.sch.id>
2024-06-13 19:32:22 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontak Kami</title>
+    <link rel="stylesheet" href="assets/css/contact.css">~
+</head>
+<body>
+    <header>
+        <h1>Kontak Kami</h1>
+    </header>
+    <section class="contact">
+        <div class="contact-info">
+            <h2>Alamat Kami:</h2>
+            <p>123 Jalan Sejahtera</p>
+            <p>Kota Harmoni</p>
+            <p>Negara Indah</p>
+            <h2>Telepon:</h2>
+            <p>085777474632
+            <h2>Email:</h2>
+            <p>bintangasyidqy@gmail.com</p>
+            <h2>Jam Kerja:</h2>
+            <p>Senin - Jumat: 08:00 - 17:00</p>
+            <p>Sabtu - Minggu: Tutup</p>
+        </div>
+        <div class="contact-form">
+            <h2>Formulir Kontak:</h2>
+            <form action="#" method="POST">
+                <label for="name">Nama:</label>
+                <input type="text" id="name" name="name" required>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <label for="message">Pesan:</label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+                <button type="submit">Kirim</button>
+            </form>
+        </div>
+    </section>
+    <footer>
+        <p>Ikuti Kami:</p>
+        <!-- Tambahkan tautan media sosial di sini -->
+    </footer>
+</body>
+</html>
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/about.css">
+</head>
+<body>
+    <div class="main">
+        <div class="img">
+            <img src="assets/img/muka.jpg">
+        </div>
+        <div class="text">
+            <h1>About Me</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, nulla placeat. Provident reiciendis voluptates et dolores a distinctio veniam dignissimos eaque, porro consequuntur nihil nisi cum ex quaerat exercitationem mollitia?
+                <br></br>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam impedit beatae perferendis tempora placeat rerum libero fuga autem corrupti quibusdam eum, reprehenderit laboriosam, accusantium cumque quaerat molestiae cum, explicabo doloremque.
+            </p>
+        </div>
+    </div>
+    
+</body>
+</html>
